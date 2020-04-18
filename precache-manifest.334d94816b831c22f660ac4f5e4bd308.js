8ce3f6728718559d28d2f175a67512b723c57457
MaryVol <meryswhite@icloud.com>
2020-04-18 16:56:40 +0300

    Updates

---
+++ precache-manifest.334d94816b831c22f660ac4f5e4bd308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653c69f3d0cf7724e01b3144577a82b4",
+    "url": "/mvsocial/index.html"
+  },
+  {
+    "revision": "5010ad24ac1b94c55c44",
+    "url": "/mvsocial/static/css/main.641fcbd4.chunk.css"
+  },
+  {
+    "revision": "b9a0036b699ece659da3",
+    "url": "/mvsocial/static/js/2.d040d7f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mvsocial/static/js/2.d040d7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5010ad24ac1b94c55c44",
+    "url": "/mvsocial/static/js/main.dbaffcfd.chunk.js"
+  },
+  {
+    "revision": "6185e835ee15d9184383",
+    "url": "/mvsocial/static/js/runtime-main.7b472751.js"
+  }
+]);
